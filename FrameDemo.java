62789832bbf547eaf7950306881d6e935791a1a4
jas-km <49551699+jas-km@users.noreply.github.com>
2019-09-12 11:27:39 +0300

    JFrame title

---
+++ FrameDemo.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import javax.awt.*;
+public class FrameDemo{
+JFrame obj;
+
+	public FrameDemo(){
+	obj=new JFrame();
+	obj.setTitle("NIIT songs");
+	obj.setVisible(true);
+	}
+
+	public static void main(String[]args){
+	FrameDemo obj= new FrameDemo();
+
+	}
+
+
+
+}
