5145610a1eeb95e237d6a16fb2ea2c9cc4061bf1
sashagift <cyresgift@gmail.com>
2019-09-12 13:27:09 +0300

    DefaultCloseOperation & Jframe

--- FrameDemo.java
+++ FrameDemo.java
@@ -7,16 +7,40 @@ JFrame obj;
 	obj=new JFrame();
 	obj.setTitle("NIIT songs");
 	obj.setVisible(true);
-	obj.setSize(300,300);
+	public class JFrameSize{
+
+private void makeFullSize(JFrame obj){
+   Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+   obj.setSize(screenSize.width,screenSize.height);
+
+}
+   
+private void makeHalfScreenSize(Jframe obj){
+   Dimention screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+   obj.setSize(screenSize.width/2,screenSize.height/2);
+
+}
+  }
 
 	obj.setLayout(new FlowLayout());
 	}
           
 	public static void main(String[]args){
 	FrameDemo obj= new FrameDemo();
+       
+        JPanel jp = new JPanel();
+        
+        JButton jb1 = new JButton("Button 1");
+        JButton jb2 = new JButton("Button 2");
+        
+        obj.add(jb1);
+        obj.add(jb2);
+       
+        obj.add(jp);
+        obj.pack();
+      
+        
 
 	}
-
-
 
 }
