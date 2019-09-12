8a98d93ebe852af846a980ff0425cff2a8896a04
sashagift <cyresgift@gmail.com>
2019-09-12 13:03:39 +0300

    Frame layout

--- FrameDemo.java
+++ FrameDemo.java
@@ -4,11 +4,12 @@ public class FrameDemo{
 JFrame obj;
 
 	public FrameDemo(){
-	obj=new JFrame();
-	obj.setTitle("NIIT songs");
-	obj.setVisible(true);
+		obj=new JFrame();
+		obj.setTitle("NIIT songs");
+		obj.setVisible(true);
+		obj.setLayout(new FlowLayout());
 	}
-
+          
 	public static void main(String[]args){
 	FrameDemo obj= new FrameDemo();
 
